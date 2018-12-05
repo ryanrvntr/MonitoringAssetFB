d3ced69ae6785ec0ad747a6dac5963ee31649ffd
ryanrvntr <ryanrvntr8@gmail.com>
2018-12-05 10:16:03 +0700

    input activity property empty

---
+++ app/src/main/java/com/example/ryanr/monitoringassetfb/Activity/PropertyActivity.java
@@ -0,0 +1,15 @@
+package com.example.ryanr.monitoringassetfb.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ryanr.monitoringassetfb.R;
+
+public class PropertyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_property);
+    }
+}
